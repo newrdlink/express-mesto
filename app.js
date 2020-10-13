9e67ca346407f716a144609b0ca038d105ce8563
Roman Didukh <newrdlink@yandex.ru>
2020-10-13 16:56:50 +0300

    add files 2

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const fs = require('fs')
+//const fs = require('fs')
 const path = require('path')
 const { PORT = 3000} = process.env
 
 const app = express()
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
-  console.log('fnh tjhоен')
+  console.log(`Наш сервер слушает ${PORT} порт!`)
 })
 
