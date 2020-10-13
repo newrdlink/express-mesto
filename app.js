2e76100b4ef3382527629c3e5c976f68a1f8cbc7
Roman Didukh <newrdlink@yandex.ru>
2020-10-13 16:47:01 +0300

    add files

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
-
+const fs = require('fs')
+const path = require('path')
 const { PORT = 3000} = process.env
 
 const app = express()
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () => {
-  console.log('fnkh tjh')
+  console.log('fnh tjhоен')
 })
+
