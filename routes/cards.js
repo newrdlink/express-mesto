87cb45c6b04318a97892aa23d831954107923104
Roman Didukh <newrdlink@yandex.ru>
2020-10-14 14:44:51 +0300

    fix after first review 2

--- routes/cards.js
+++ routes/cards.js
@@ -7,7 +7,7 @@ const jsonCardsData = path.join(__dirname, '..', 'data', 'cards.json')
 router.get('/', (req, res) => {
   readFile(jsonCardsData)
     .then(cardsData => res.send(cardsData))
-    .catch(res.status(500).send({message: 'Ошибка чтения файла'}))
+    .catch(res.status(500).send({ message: 'Ошибка чтения файла' }))
 })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/users', usersRoutes)
 router.use('/cards', cardsRoutes)
 router.use('*', (req, res) => {
   const { '0': badPath } = req.params
-  res.send({ "message": `Веденный путь: ${badPath} - не допустимый`})
+  res.status(404).send({ "message": `Веденный путь: ${badPath} - не допустимый` })
 })
 
 module.exports = router;
