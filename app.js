3c19663dfed44f3adc1bc93fb34c819fee1f68e7
Roman Didukh <newrdlink@yandex.ru>
2020-10-15 00:13:07 +0300

    fix last after first review 2

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const { PORT = 3000 } = process.env
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+const { PORT = 3000 } = process.env;
 // make router from file
-const router = require('./routes/index.js')
+const router = require('./routes/index.js');
 // sharing folder for everyone
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 // start router
-app.use(router)
+app.use(router);
 // start server
 app.listen(PORT, () => {
-  console.log(`Наш сервер слушает ${PORT} порт`)
-})
-
+  console.log(`Наш сервер слушает ${PORT} порт`);
+});
