97b4e844f8775fa6445f8ca3588a97a54d3e0e53
Roman Didukh <newrdlink@yandex.ru>
2020-10-29 21:26:40 +0300

    add schema user 2

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
 
   name: {
     type: String,
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,40 @@
 const router = require('express').Router();
-const path = require('path');
-const readFile = require('../utils/read-file');
+// const path = require('path');
+const User = require('../models/user');
 
-const jsonUsersData = path.join(__dirname, '..', 'data', 'users.json');
-
-router.get('/', (req, res) => {
-  readFile(jsonUsersData)
-    .then((usersData) => res.send(usersData))
-    .catch(() => res.status(500).send({ message: 'Ошибка чтения файла' }));
+router.post('/', (req, res) => {
+  const { name, about, avatar } = req.body;
+  console.log(name);
+  User.create({ name, about, avatar })
+    .then((newUser) => res.send(newUser))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка2' }));
 });
+// const readFile = require('../utils/read-file');
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  readFile(jsonUsersData)
-    .then((usersData) => {
-      const userFind = usersData.find((user) => user._id === id);
-      return userFind;
-    })
-    .catch(() => res.status(500).send({ message: 'Ошибка чтения файла' }))
-    .then((userFind) => {
-      if (!userFind) {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
-      }
-      return res.send(userFind);
-    })
-    .catch(() => res.status(500).send({ message: 'Ошибка чтения файла' }));
-});
+// const jsonUsersData = path.join(__dirname, '..', 'data', 'users.json');
+
+// router.get('/', (req, res) => {
+//   readFile(jsonUsersData)
+//     .then((usersData) => res.send(usersData))
+//     .catch(() => res.status(500).send({ message: 'Ошибка чтения файла' }));
+// });
+
+// router.get('/:id', (req, res) => {
+//   const { id } = req.params;
+//   readFile(jsonUsersData)
+//     .then((usersData) => {
+//       const userFind = usersData.find((user) => user._id === id);
+//       return userFind;
+//     })
+//     .catch(() => res.status(500).send({ message: 'Ошибка чтения файла' }))
+//     .then((userFind) => {
+//       if (!userFind) {
+//         return res.status(404).send({ message: 'Нет пользователя с таким id' });
+//       }
+//       return res.send(userFind);
+//     })
+//     .catch(() => res.status(500).send({ message: 'Ошибка чтения файла' }));
+// });
+
 
 module.exports = router;
