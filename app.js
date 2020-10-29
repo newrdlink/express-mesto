c83cf535bb1c6b83bef729f1a55da6abc31ee5f2
Roman Didukh <newrdlink@yandex.ru>
2020-10-29 13:42:20 +0300

    add connect to bd

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const { PORT = 3000 } = process.env;
 // make router from file
